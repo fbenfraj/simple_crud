ef14b3f6416e096455766dbc6de298e041f54df5
BEN FRAJ <farouk.ben-fraj@sogeti.com>
2019-05-07 15:27:42 +0200

    turned ReadValues into a functionnal component

--- simple_crud_front/src/components/ReadValues.js
+++ simple_crud_front/src/components/ReadValues.js
@@ -1,39 +1,15 @@
 import React from "react";
 
-export class ReadValues extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            valuesMap: {}
-        }
-
-        this.appendOptions = this.appendOptions.bind(this);
-    }
-
-    handleClick(e) {
-        console.log("handleClick for delete called!");
-    }
-
-    appendOptions() {
-        for(let i = 0; i < this.state.valuesArray.length; i++) {
-            const option = document.createElement("option");
-            option.value = this.state.valuesArray[i];
-            option.innerHTML = this.state.valuesArray[i];
-            document.getElementById("readValues").append(option);
-            }
-    }
-
-    render() {
-        return(
-            <div>
-                <label>Database values: </label>
-                <select>
-                    { this.props.valuesArray.map((value, index) => 
-                        <option key={index} value={value}>{value.value}</option>)
-                    };
-                </select>
-                <button onClick={this.handleClick}>Delete</button>
-            </div>
-        );
-    }
+export function ReadValues(props) {
+    return(
+        <div>
+            <label>Database values: </label>
+            <select>
+                { props.valuesArray.map((value, index) => 
+                    <option key={index} value={value}>{value.value}</option>)
+                };
+            </select>
+            <button>Delete</button>
+        </div>
+    );
 }
